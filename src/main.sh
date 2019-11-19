43ff4d66bdd9f9ac912216c131e0abd715ac14f7
Jack <jackskjuda@gmail.com>
2019-11-19 11:03:52 -0500

    corrects gcreds

--- src/main.sh
+++ src/main.sh
@@ -5,6 +5,8 @@ export GOOGLE_APPLICATION_CREDENTIALS=/var/sec/gcp_cred.json
 mkdir -p /var/sec
 echo $GOOGLE_CREDENTIALS > /var/sec/gcp_cred.json
 
+cat /var/sec/gcp_cred.json
+
 function stripColors {
   echo "${1}" | sed 's/\x1b\[[0-9;]*m//g'
 }
